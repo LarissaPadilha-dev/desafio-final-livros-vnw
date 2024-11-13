b61abaf837e2e10139f21e2ce5b87df37edbb617
LarissaPadilha-dev <larissapadilha436@gmail.com>
2024-11-13 18:27:36 -0300

    :tada: Commit inicial

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,17 @@
+
+
+export default function header() {
+  return (
+    <header>
+        <section>
+            <img src="" alt="" />
+            <h1>Livros VNW</h1>
+        </section>
+        <nav>
+          <a href="">Início</a>
+          <a href="">Livros Doados</a>
+          <a href="">Quero Doar</a>
+        </nav>
+    </header>
+  )
+}
